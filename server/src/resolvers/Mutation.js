27eaa3ee6b2de8d01d9c67f19f95a1ff2ca6a697
Mousa Ayoubi <mousaayoubi@gmail.com>
2020-09-22 14:39:43 +0000

    Update files

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -2,12 +2,18 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
-function post(parent, args, context) {
-  return context.prisma.createLink({
-    url: args.url,
-    description: args.description,
-  })
-}
+function post(parent, { url, description }, context) {
+	const userId = getUserId(context)
+	return context.prisma.createLink({
+		url,
+		description,
+		postedBy: {
+			connect: {
+				id: userId
+			}
+		}
+	})
+};
 
 async function signup(parent, args, context) {
   const password = await bcrypt.hash(args.password, 10)
--- src/index.js
+++ src/index.js
@@ -9,14 +9,26 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
 
 const httpLink = createHttpLink({
 	// Update url if needed
 	uri: 'http://35.242.255.189:4000/'
 });
 
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN)
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ''
+		}
+	}
+});
+
 const client = new ApolloClient({
-	link: httpLink,
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache()
 });
 
