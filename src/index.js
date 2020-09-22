c3afde67a71d4fac421a038da3d9222b5f695503
Mousa Ayoubi <mousaayoubi@gmail.com>
2020-09-22 11:26:09 +0000

    Update files

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+	// Update url if needed
+	uri: 'http://35.242.255.189:4000/'
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+	  <App />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
+
+serviceWorker.unregister();
