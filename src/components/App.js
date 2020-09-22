953a610ae703640bb7334e50255cd0a830a4f9dc
Mousa Ayoubi <mousaayoubi@gmail.com>
2020-09-22 13:08:58 +0000

    Update files

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,18 @@ import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
-  return (
-    <div className="center w85">
-      <Header />
-      <div className="ph3 pv1 background-gray">
-        <Switch>
-          <Route exact path="/" component={LinkList} />
-          <Route exact path="/create" component={CreateLink} />
-        </Switch>
-      </div>
-    </div>
-  )
-}
+	  return (
+		  <div className="center w85">
+		    <Header />
+		    <div className="ph3 pv1 background-gray">
+		     <Switch>
+		      <Route exact path="/" component={LinkList} />
+		      <Route exact path="/create" component={CreateLink} />
+		     </Switch>
+		     </div>
+		  </div>
+	  )
+  }
 };
 
 export default App;
